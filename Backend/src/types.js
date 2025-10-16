c857f23d8e28980d7181f35b87667d50d76d7ff5
Rajeshvissa <126651798+Rajeshvissa@users.noreply.github.com>
2025-10-16 19:51:11 +0530

    Update types.js

--- Backend/src/types.js
+++ Backend/src/types.js
@@ -1,13 +1,3 @@
-/**
- * @typedef {Object} TaskDoc
- * @property {string} _id
- * @property {string} goalId
- * @property {string} title
- * @property {string=} description
- * @property {number} estimateDays
- * @property {string[]} dependsOn
- * @property {string|Date} startDate
- * @property {string|Date} endDate
- * @property {"todo"|"in_progress"|"done"} status
- */
+
 export {};
+
